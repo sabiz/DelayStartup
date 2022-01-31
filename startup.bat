3295b94f97f8b75f45c44fb40cd0873090dc6616
sAbIz <bellett@sabiz.jp>
2022-01-31 10:11:20 +0900

    Update

--- startup.bat
+++ startup.bat
@@ -1,9 +1,13 @@
 @echo off
-REM --------------------------------------------
-REM  Delay start-up bat
-REM  v 0.0.1
-REM --------------------------------------------
-setlocal
+echo      ____       __                    __             __                       __          __  
+echo     / __ \___  / /___ ___  __   _____/ /_____ ______/ /_      __  ______     / /_  ____ _/ /_ 
+echo    / / / / _ \/ / __ `/ / / /  / ___/ __/ __ `/ ___/ __/_____/ / / / __ \   / __ \/ __ `/ __/ 
+echo   / /_/ /  __/ / /_/ / /_/ /  (__  ) /_/ /_/ / /  / /_/_____/ /_/ / /_/ /  / /_/ / /_/ / /_   
+echo  /_____/\___/_/\__,_/\__, /  /____/\__/\__,_/_/   \__/      \__,_/ .___/  /_.___/\__,_/\__/   
+echo                     /____/                                      /_/                    V 1.0.0
+echo.
+
+setlocal enabledelayedexpansion
 
 set APP_LOCATION=%SystemDrive%\Users\%USERNAME%\startup
 set STARTUP_PATH=%SystemDrive%\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
@@ -27,10 +31,17 @@ if %ERRORLEVEL% neq 0 (
     exit 0
 )
 
-timeout 60
+set idx=1
 cd %APP_LOCATION%
 for %%a in (*.lnk) do (
-  start cmd /C start %%a
-  timeout 30 > nul
+    if !idx! gtr 1 (
+        echo waitng 30 sec... [%%~na]
+        timeout 30 > nul
+    ) else (
+        echo waitng 60 sec... [%%~na]
+        timeout 60 > nul
+    )
+    start cmd /C tart %%a
+    set /a idx=!idx!+1
 )
 endlocal
