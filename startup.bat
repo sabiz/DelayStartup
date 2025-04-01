4dd4b4f2a6cd52d0e6c387053d2a2e78994f3030
sAbIz <sabiz@users.noreply.github.com>
2025-04-01 10:39:30 +0900

    Update startup.bat
    Support windows store app

--- startup.bat
+++ startup.bat
@@ -4,19 +4,19 @@ echo     / __ \___  / /___ ___  __   _____/ /_____ ______/ /_      __  ______   
 echo    / / / / _ \/ / __ `/ / / /  / ___/ __/ __ `/ ___/ __/_____/ / / / __ \   / __ \/ __ `/ __/ 
 echo   / /_/ /  __/ / /_/ / /_/ /  (__  ) /_/ /_/ / /  / /_/_____/ /_/ / /_/ /  / /_/ / /_/ / /_   
 echo  /_____/\___/_/\__,_/\__, /  /____/\__/\__,_/_/   \__/      \__,_/ .___/  /_.___/\__,_/\__/   
-echo                     /____/                                      /_/                    V 1.0.0
+echo                     /____/                                      /_/                    V 1.5.0
 echo.
 
 setlocal enabledelayedexpansion
 
-set APP_LOCATION=%USERPROFILE%\startup
-set STARTUP_PATH=%APPDATA%\Microsoft\Windows\Start Menu\Programs\StartUp
+set APP_LOCATION=%SystemDrive%\Users\%USERNAME%\startup
+set STARTUP_PATH=%SystemDrive%\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 set SELF_FILE=%0
 set SELF_FILE_NAME=%~nx0
 
 if not exist "%STARTUP_PATH%\%SELF_FILE_NAME%" (
     echo Install Startup bat...
-    copy /Y %SELF_FILE_NAME% "%STARTUP_PATH%\%SELF_FILE_NAME%" > nul
+    copy /Y "%SELF_FILE_NAME%" "%STARTUP_PATH%\%SELF_FILE_NAME%" > nul
     echo Install done.
     pause
     exit 0
@@ -25,23 +25,33 @@ if not exist "%STARTUP_PATH%\%SELF_FILE_NAME%" (
 fc %SELF_FILE% "%STARTUP_PATH%\%SELF_FILE_NAME%" > nul
 if %ERRORLEVEL% neq 0 (
     echo Update Startup bat...
-    copy /Y %SELF_FILE% "%STARTUP_PATH%\%SELF_FILE_NAME%" > nul
+    copy /Y "%SELF_FILE_NAME%" "%STARTUP_PATH%\%SELF_FILE_NAME%" > nul
     echo Install done.
     pause
     exit 0
 )
 
 set idx=1
-cd "%APP_LOCATION%"
-for %%a in (*.lnk) do (
-    if !idx! gtr 1 (
-        echo waitng 30 sec... [%%~na]
-        timeout 30 > nul
+cd %APP_LOCATION%
+for %%a in (*.lnk, *.txt) do (
+    if "%%~xa"==".lnk" (
+        if !idx! gtr 1 (
+            echo waitng 30 sec... [%%~na]
+            timeout 30 > nul
+        ) else (
+            echo waitng 60 sec... [%%~na]
+            timeout 60 > nul
+        )
+        start cmd /C start %%a
+        set /a idx=!idx!+1
     ) else (
-        echo waitng 60 sec... [%%~na]
-        timeout 60 > nul
+        setlocal disabledelayedexpansion
+        for /f "usebackq delims=\ tokens=*" %%l in (%%a) do (
+            echo waitng 30 sec... [%%~na]
+            timeout 30 > nul
+            cmd /c start shell:Appsfolder\%%l
+        )
+        setlocal enabledelayedexpansion
     )
-    start cmd /C start "" """%%a"""
-    set /a idx=!idx!+1
 )
 endlocal
